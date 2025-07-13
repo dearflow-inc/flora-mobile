c15f54cc229a7d73b0c9f5603f7e7970aaa3da41
GitHub Action <action@github.com>
2025-07-13 10:33:14 +0000

    Bump version for dev build

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
   expo: {
     name: "DearFlow",
     slug: "flora-mobile",
-    version: "1.0.0",
+    version: "1.0.01",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "ai.dearflow.email",
